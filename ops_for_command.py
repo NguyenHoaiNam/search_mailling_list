db78041255a5ef69d18f902896b4c70aa9ea15a3
Nguyen Hoai Nam <namptit307@gmail.com>
2017-01-09 23:59:27 +0700

    Update ops_for_command.py

--- ops_for_command.py
+++ ops_for_command.py
@@ -7,13 +7,14 @@ def option_command():
     parser = optparse.OptionParser(usage="usage: %prog [options]",
                                    version="%prog 1.0")
     parser.add_option("-m", "--month", action='store', type="int",
-                      dest="month", help="A month which need to seach")
+                      dest="month", default=None,
+                      help="A month which need to seach")
     parser.add_option("-sm", "--start-month", action='store', type="int",
-                      dest="start_month", help="Start month")
+                      dest="start_month", default=None, help="Start month")
     parser.add_option("-em", "--end-month", action='store', type="int",
-                      dest="end_month", help="End month")
+                      dest="end_month", default=None, help="End month")
     parser.add_option("-y", "--year", action='store', type="int",
-                      dest="year", help="Year", default="2016")
+                      dest="year", help="Year", default=2016)
     parser.add_option("-k", "--keyword", action='store', type="string",
                       dest="keyword", help="Keyword to search")
     (options, args) = parser.parse_args()
