03c3ba3ac9bd5fec308f63247eb854a0458f470d
Nguyen Hoai Nam <namptit307@gmail.com>
2017-01-09 23:56:20 +0700

    Create ops_for_command.py

---
+++ ops_for_command.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Nguyen Hoai Nam
+
+def option_command():
+    global LF
+    parser = optparse.OptionParser(usage="usage: %prog [options]",
+                                   version="%prog 1.0")
+    parser.add_option("-m", "--month", action='store', type="int",
+                      dest="month", help="A month which need to seach")
+    parser.add_option("-sm", "--start-month", action='store', type="int",
+                      dest="start_month", help="Start month")
+    parser.add_option("-em", "--end-month", action='store', type="int",
+                      dest="end_month", help="End month")
+    parser.add_option("-y", "--year", action='store', type="int",
+                      dest="year", help="Year", default="2016")
+    parser.add_option("-k", "--keyword", action='store', type="string",
+                      dest="keyword", help="Keyword to search")
+    (options, args) = parser.parse_args()
+    month = options.month
+    start_month = options.start_month
+    end_month = options.end_month
+    year = options.year
+    keyword = options.keyword
