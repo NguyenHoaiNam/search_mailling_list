75074bcdeb3b7831d51ac47241bedaa86769e511
Hoai Nam <namptit307@gmail.com>
2017-01-10 23:40:33 +0700

    Update 1

--- search_what.py
+++ search_what.py
@@ -12,18 +12,32 @@ MAPPING_MONTH = {
 
 
 def get_url(month_number, year):
-    month_string = MAPPING_MONTH[month_number]
+    month_string = MAPPING_MONTH[int(month_number)]
     url = "http://lists.openstack.org/pipermail/" \
-          "openstack-dev/%s-%s/thread.html" % (month_string, year)
-    return requests.get(url)
+          "openstack-dev/%s-%s/thread.html" % (year, month_string)
+    get_request = requests.get(url)
+    content_request = get_request.content
+    return content_request.split('\n')
 
 
 def my_heart(content, list_filter):
     result = []
+    list_filter_upper = [member.upper() for member in list_filter]
     for i in content:
-        if all(list_filter in i for list_filter in list_filter):
-            result.append(i)
+        if i.startswith('<LI><A'):
+            i_upper = i.upper()
+            if all(word in i_upper for word in list_filter_upper):
+                from_index = i.find('[openstack-dev]')
+                result.append(i[from_index:])
     return result
+
+
+def action_filter(month, year, keywords):
+    content_ml = get_url(month, year)
+    result_filter = my_heart(content_ml, keywords)
+    print '#### The result for %s' % MAPPING_MONTH[month]
+    for i in result_filter:
+        print i
 
 
 def main():
@@ -32,11 +46,11 @@ def main():
     parser.add_option("-m", "--month", action='store', type="int",
                       dest="month", default=None, metavar="MONTH",
                       help="A month which need to seach")
-    parser.add_option("-sm", "--start-month", action='store',
+    parser.add_option("-s", "--start-month", action='store',
                       type="int", dest="start_month",
                       metavar="START_MONTH",
                       default=None, help="Start month")
-    parser.add_option("-em", "--end-month", action='store',
+    parser.add_option("-e", "--end-month", action='store',
                       type="int", dest="end_month", default=None,
                       metavar="END_MONTH", help="End month")
     parser.add_option("-y", "--year", action='store', type="int",
@@ -50,7 +64,14 @@ def main():
     start_month = options.start_month
     end_month = options.end_month
     year = options.year
-    keyword = options.keywords
-    
+    keywords = options.keywords
+    if month:
+        action_filter(month, year, keywords)
+    else:
+        range_month = range(int(start_month), int(end_month) + 1)
+        for month in range_month:
+            action_filter(month, year, keywords)
+
+
 if __name__ == "__main__":
     main()
