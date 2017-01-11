313f32209768fbcbfe003a3ddaf215a52198083d
Nam Nguyen Hoai <namnh@vn.fujitsu.com>
2017-01-11 11:36:40 +0700

    Update2

--- search_what.py
+++ search_what.py
@@ -22,22 +22,27 @@ def main():
     parser = optparse.OptionParser(usage="usage: %prog [options]",
                                    version="%prog 1.0")
     parser.add_option("-m", "--month", action='store', type="int",
-                      dest="month", default=None,
+                      dest="month", default=None, metavar="MONTH",
                       help="A month which need to seach")
-    parser.add_option("-sm", "--start-month", action='store', type="int",
-                      dest="start_month", default=None, help="Start month")
-    parser.add_option("-em", "--end-month", action='store', type="int",
-                      dest="end_month", default=None, help="End month")
+    parser.add_option("-sm", "--start-month", action='store',
+                      type="int", dest="start_month",
+                      metavar="START_MONTH",
+                      default=None, help="Start month")
+    parser.add_option("-em", "--end-month", action='store',
+                      type="int", dest="end_month", default=None,
+                      metavar="END_MONTH", help="End month")
     parser.add_option("-y", "--year", action='store', type="int",
+                      metavar="YEAR",
                       dest="year", help="Year", default=2016)
-    parser.add_option("-k", "--keyword", action='store', type="string",
-                      dest="keyword", help="Keyword to search")
+    parser.add_option("-k", "--keyword", action='append',
+                      type="string", dest="keywords",
+                      metavar="KEYWORDS", help="Keyword to search")
     (options, args) = parser.parse_args()
     month = options.month
     start_month = options.start_month
     end_month = options.end_month
     year = options.year
-    keyword = options.keyword
+    keyword = options.keywords
     
 if __name__ == "__main__":
     main()
